7ed6dbacf619f2da3d1f25df9c9466e557abca8e
Himanshu Tyagi <thimanshu937@gmail.com>
2024-10-13 02:28:43 +0000

    initial commit where we setupped the container

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("Heyy!!! JavaScript");
+console.log(" this is javascript ");
+
+let name = "neha"
+let standard = "BCA"
+
+for (let index = 0; index < 100; index++) {
+     console.log(name+" studies in "+standard);
+     
+    
+}
